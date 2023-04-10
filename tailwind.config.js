5fd87cd9bbd962573a2d15f306fa02fef748fee3
BlckTitan <dorshman406@gmail.com>
2023-04-10 16:27:57 +0100

    Defined the media query presets

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      'sm': '640px',
+      // => @media (min-width: 640px) {}
+
+      'md': '768px',
+      // => @media (min-width: 768px) {}
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) {}
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) {}
+    },
   },
   plugins: [],
 }
